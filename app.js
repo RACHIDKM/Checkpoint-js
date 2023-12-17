fa47a577964badd6a70f7459cf4e7f3cf45600b4
RACHIDKM <rkarym7@gmail.com>
2023-12-17 15:05:36 +0100

    js

---
+++ app.js
@@ -0,0 +1,34 @@
+function add(num1, num2) {
+    let total = num1 + num2;
+    return total;
+}
+
+console.log(add(25, 12));
+
+function subtract(num1, num2) {
+    let difference = num1 - num2;
+    return difference;
+}
+
+console.log(subtract(1, 12));
+
+function multiply(num1, num2) {
+    let product = num1 * num2;
+    return product;
+}
+
+console.log(multiply(6, 2));
+
+function division(num1, num2) {
+    let product = num1 / num2;
+    return product;
+}
+
+console.log(division(18, 8));
+
+function modulo(num1, num2) {
+    let product = num1 % num2;
+    return product;
+}
+
+console.log(modulo(6, 2));
